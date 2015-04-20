307295a81ccc763f98e241f4e618326ce2999486
sunlnx <tianzhiwu2010@gmail.com>
2015-04-20 16:56:02 +0800

    imporve zipcomrer

--- hustocsfileser/src/net/SunLnx/Comprer/ZipComprer.java
+++ hustocsfileser/src/net/SunLnx/Comprer/ZipComprer.java
@@ -1,10 +1,21 @@
 package net.SunLnx.Comprer;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.zip.ZipOutputStream;
+
 public class ZipComprer implements Comprer{
 
 	@Override
 	public boolean compress(String srcpath, String desfile) {
-		// TODO Auto-generated method stub
+		File f = new File(desfile);
+		try {
+			ZipOutputStream zis = new ZipOutputStream(new FileOutputStream(f));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 		return false;
 	}
 
