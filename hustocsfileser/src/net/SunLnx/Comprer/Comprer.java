f6449c7650075d7e3900977908fcedcec6c25081
sunlnx <tianzhiwu2010@gmail.com>
2015-04-21 09:37:26 +0800

    add compress and decompress class

--- hustocsfileser/src/net/SunLnx/Comprer/Comprer.java
+++ hustocsfileser/src/net/SunLnx/Comprer/Comprer.java
@@ -1,5 +1,8 @@
 package net.SunLnx.Comprer;
 
+import java.util.List;
+
 public interface Comprer {
 	public boolean compress(String srcpath, String desfile);
+	public boolean comress(List<String> srcpaths, String desfile);
 }
